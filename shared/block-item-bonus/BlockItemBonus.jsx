2feef73da80187adbed1bd69631be5d2cc3e7377
Marian Polyak <info@neo-lines.com>
2024-07-03 02:14:38 +0300

    Changed bonuses

--- shared/block-item-bonus/BlockItemBonus.jsx
+++ shared/block-item-bonus/BlockItemBonus.jsx
@@ -1,6 +1,7 @@
 import './block-item-bonus.scss'
 import './media.scss'
 import Link from "next/link";
+import Image from 'next/image';
 import {BASIS_URL} from "../../app/config/config";
 import React from "react";
 import {sizeText} from "../../app/info/info";
@@ -14,9 +15,21 @@ const BlockItemBonus = ({item}) => {
         <Link className='block-item-bonus'
               href={item?.node?.uri}>
             <div className="block-item-bonus__image">
-                <img className="block-item-bonus__image-img" src={`${BASIS_URL}/${item?.node?.AcfBonus?.imageAnons?.uri}`} alt={item?.node?.AcfBonus?.imageAnons?.altText}/>
+                <div className="block-item-bonus__image-img">
+                    <Image
+                        src={item?.node?.AcfBonus?.imageAnons?.sourceUrl}
+                        alt={item?.node?.AcfBonus?.imageAnons?.altText}
+                        width={250}
+                        height={130}
+                        layout="intrinsic"
+                    />
+                </div>
+                {/*<img className="block-item-bonus__image-img"*/}
+                {/*     src={`${BASIS_URL}/${item?.node?.AcfBonus?.imageAnons?.uri}`}*/}
+                {/*     alt={item?.node?.AcfBonus?.imageAnons?.altText}/>*/}
                 <div className="block-item-bonus__image-title">{trimText(item?.node?.title, sizeText.ma)}</div>
-                <div className="block-item-bonus__image-short">{trimText(item?.node?.AcfBonus?.titleShort, sizeText.ma)}</div>
+                <div
+                    className="block-item-bonus__image-short">{trimText(item?.node?.AcfBonus?.titleShort, sizeText.ma)}</div>
                 <div className="block-item-bonus__image-overlay"></div>
             </div>
         </Link>
